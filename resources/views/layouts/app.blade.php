54fc643a4f09f5025795c93ad30f0cebbf5e07b2
Kacper <kgrzesik17@gmail.com>
2025-04-29 22:44:35 +0200

    styling flash messages with alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
@@ -34,12 +35,26 @@
 
     @yield('styles')
 </head>
+
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
     <h1 class="mb-4 text-2xl">@yield('title')</h1>
 
-    <div>
+    <div x-data="{ flash: true }">
         @if(session()->has('success'))
-            <div>{{ session('success') }}</div>
+            <div x-show="flash"
+                class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>{{ session('success') }}</div>
+
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" @click="flash = false"
+                        stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
         @endif
 
         @yield('content')
