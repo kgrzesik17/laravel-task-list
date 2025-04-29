f2742ac796931046e643689485ab0c5969f118d5
Kacper <kgrzesik17@gmail.com>
2025-04-29 21:59:17 +0200

    adding styles with tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     @yield('styles')
 </head>
-<body>
-    <h1>@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="mb-4 text-2xl">@yield('title')</h1>
 
     <div>
         @if(session()->has('success'))
