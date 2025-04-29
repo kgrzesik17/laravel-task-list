ae98cd6a6ab9b53b80d19ae1632f0d549f51163b
Kacper <kgrzesik17@gmail.com>
2025-04-29 21:45:44 +0200

    toggling task statew

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/tasks/{task}/edit', function(Task $task) {
     return view('edit', [
         'task' => $task
     ]);
-});
+})->name('tasks.edit');
 
 Route::get('/tasks/{task}', function(Task $task){
     return view('show', ['task' => $task]);
@@ -49,6 +49,12 @@ Route::delete('/tasks/{task}', function(Task $task) {
     return redirect()->route('tasks.index')
         ->with('success', 'Task deleted successfully!');
 })->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function(Task $task) {
+    $task->toggleComplete();
+
+    return redirect()->back()->with('success', 'Task updated successfully');
+})->name('tasks.toggle-complete');
 
 // 404
 Route::fallback(function() {
